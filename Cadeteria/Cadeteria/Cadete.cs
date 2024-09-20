ff35b2452d8e4cbaf8cc28f6746d119f5925b023
AnA <analuciamrls@gmail.com>
2024-09-20 10:41:04 -0300

    quito listado de pedidos de cadetes y los métodos que ya no son relevantes

--- Cadeteria/Cadeteria/Cadete.cs
+++ Cadeteria/Cadeteria/Cadete.cs
@@ -7,16 +7,14 @@
         private string _address;
         private string _phone;
         private int _orderCount;
-        private List<Pedido> _pedidos;
 
         public Cadete(int id, string name, string address, string phone)
         {
-            _id = id;
-            _name = name;
-            _address = address;
-            _phone = phone;
-            _orderCount = 0;
-            _pedidos = new List<Pedido>();
+            Id = id;
+            Name = name;
+            Address = address;
+            Phone = phone;
+            OrderCount = 0;
         }
 
         public Cadete()
@@ -28,41 +26,12 @@
         public string Address { get => _address; set => _address = value; }
         public string Phone { get => _phone; set => _phone = value; }
         public int OrderCount { get => _orderCount; set => _orderCount = value; }
-        internal List<Pedido> Pedidos { get => _pedidos; set => _pedidos = value; }
-
-        public int JornalACobrar() => _orderCount * 500;
-
-        public void AgregarPedido(Pedido pedido)
-        {
-            Pedidos.Add(pedido);
-        }
-
-        public bool CambiarEstadoPedido(int nroPedido, Estado estado)
-        {
-            Pedido? pedido = Pedidos.FirstOrDefault(x => x.Number == nroPedido);
-
-            if (pedido != null)
-            {
-                if (estado != Estado.Pendiente)
-                {
-                    pedido.CambiarEstado(estado);
-                    OrderCount++;                    
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
-        }
 
         public void MostrarDatosInforme()
         {
             Console.WriteLine($"ID: {this.Id}");
             Console.WriteLine($"Nombre: {this.Name}");
             Console.WriteLine($"Pedidos entregados: {this.OrderCount}");
-            Console.WriteLine($"Monto ganado: {this.JornalACobrar()}");
         }
 
     }
